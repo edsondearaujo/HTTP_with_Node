f2380ec33e6d61d66a2258bb7be1671ea4e8d03e
edsondearaujo <edsondearaujofga@gmail.com>
2021-06-18 18:47:39 -0300

    fix(#4): fix bug, file exists

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log("Request for " + req.url + 'by method ' );
+    console.log("Request for " + req.url + 'by method ' + req.method );
 
-    if(req.method == 'GET'){
+    if (req.method == 'GET') {
         var fileUrl;
-        if(req.url == '/') fileUrl = '/index.html';
+        if (req.url == '/') fileUrl = '/index.html';
         else fileUrl = req.url;
 
         var filePath = path.resolve('./public' + fileUrl);
         const fileExt = path.extname(filePath);
-        if(fileExt == '.html'){
-            fs.exists(filePath, (fs.exists) => {
-                if(!fs.exists){
+        if (fileExt == '.html') {
+            fs.createWriteStream(filePath, (exists) => {
+                if (!exists) {
                     res.statusCode = 404;
                     res.setHeader('Content-Type', 'text');
                     res.end('<html><body><h1>Error 404: ' + fileUrl + 
@@ -27,14 +27,24 @@ const server = http.createServer((req, res) => {
                 res.statusCode = 200;
                 res.setHeader('content-Type', 'text/html');
                 fs.createReadStream(filePath).pipe(res);            
-            })
+            });
         }
-
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text');
+            res.end('<html><body><h1>Error 404: ' + fileUrl + 
+            ' not an HTML file</h1></body></html>');
+            
+            return;
+        }
     }
-
-    res.statusCode = 200;
-    res.setHeader('content-Type', 'text/html');
-    fs.createReadStream(filePath).pipe(res);
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text');
+        res.end('<html><body><h1>Error 404: ' + req.method + 
+        ' not supported</h1></body></html>');
+        return;
+    }
 })
 
 server.listen(port, hostname, () => {
